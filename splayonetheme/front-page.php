7b2639b87003f70f5703b6fd403f11e344843ff7
Hadla Bergman <hadla.bergman@einride.tech>
2023-05-11 21:33:46 +0200

    feat: add all sections to front-page

--- splayonetheme/front-page.php
+++ splayonetheme/front-page.php
@@ -2,12 +2,16 @@
     <section>
         <div class="page_container">
             
-          
+            <div class="content">
+                <?php get_template_part('includes/section', 'intro'); ?>
+            </div>
+
+            <?php get_template_part('includes/section', 'awards' ); ?>
 
             <div class="content">
                 <div class="thumbnail_container d-flex flex-row flex-wrap">
-                        <?php
-                    // Custom query to retrieve Profiles post type posts
+                    <?php
+                    // Custom query to retrieve Cases post type posts
                     $cases_args = array(
                         'post_type' => 'cases', // Profiles post type slug
                         'posts_per_page' => -1, // Number of posts to retrieve (-1 for all posts)
@@ -53,7 +57,49 @@
                 </div>
             </div>
 
+            <?php get_template_part('includes/section', 'contact' ); ?>
 
+            
+            <?php
+                    $news_args = array(
+                        'post_type' => 'news_articles', 
+                        'posts_per_page' => -1,
+                    );
+                    $news_query = new WP_Query($news_args);
+
+                    if ($news_query->have_posts()) {
+                        ?>
+                        <div class="content home_news_section">
+                            <p class="title_2">News</p>
+                            <div class="news_scrool_wrapper">
+                                <div class="news_scrool_feed">
+                                    <?php
+                                    while ($news_query->have_posts()) {
+                                        $news_query->the_post();
+
+                                        $news_title = get_post_meta(get_the_ID(), 'news_title', true);
+                                        $news_text = get_post_meta(get_the_ID(), 'news_text', true);
+                                        $news_date = get_post_meta(get_the_ID(), 'news_date', true);
+                                        $news_link = get_post_meta(get_the_ID(), 'news_link_url', true);
+                                        ?>
+                                            <a target="_blank" href="<?php echo $news_link;?>" class="news_item">
+                                                <div>
+                                                    <p class="font-weight-bold adieu_light text_2"><?php echo $news_date; ?></p>
+                                                    <p class="text-uppercase text_2 "><?php echo $news_title; ?></p>
+                                                </div>
+                                                <p class="text_2"><?php echo $news_text; ?></p>
+                                            </a>
+                                        <?php
+                                    }
+                                    wp_reset_postdata();
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php
+                    } 
+                ?>
+            
         </div>
     </section>
 <?php get_footer(); ?>
