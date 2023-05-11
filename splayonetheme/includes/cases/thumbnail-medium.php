13493fa125aaaca3fa466b4a6e26a54c3ce789e5
Hadla Bergman <hadla.bergman@einride.tech>
2023-05-12 00:35:41 +0200

    fix: front-page fixes

--- splayonetheme/includes/cases/thumbnail-medium.php
+++ splayonetheme/includes/cases/thumbnail-medium.php
@@ -1,9 +1,9 @@
-<div class="col-md-8">
+<div class="col-md-8 p-0 thumbnail_item">
     <?php if (!empty($args['data']['image_url'])) : ?>
         <img class="case_img" src="<?php echo esc_url($args['data']['image_url']);?>" alt="<?php echo $args['data']['case_name']; ?>"/>
     <?php endif; ?>
     <div class="d-flex flex-row pt-2">
-        <p class="text-uppercase adieu_regular pr-3"><?php echo $args['data']['customer']; ?></p>
-        <p class="font-weight-bold adieu_light"><?php echo $args['data']['case_name']; ?></p>
+        <p class="text-uppercase adieu_regular pr-3"><?php echo $args['data']['client']; ?></p>
+        <p class="adieu_light"><?php echo $args['data']['case_name']; ?></p>
     </div>
 </div>
