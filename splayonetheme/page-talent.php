5d69cde0889db0ba1524cdb8c7fdaa8913304130
Hadla Bergman <hadla.bergman@einride.tech>
2023-05-15 16:30:16 +0200

    feat: add vimeo implementation to talent page

--- splayonetheme/page-talent.php
+++ splayonetheme/page-talent.php
@@ -11,13 +11,22 @@ Template Name: Talent Page
 <!-- SECTION: INTRO -->
             <?php 
             $header_image = get_field('talent_header_image');
+            $talent_vimeo_id = get_field('talent_header_vimeo_id');
             ?>
-            <div class="about_header">
                 <?php 
                 if( !empty( $header_image ) ): ?>
+                    <div class="about_header">
+                        <img class="about_header_image" src="<?php echo esc_url($header_image['url']); ?>" />
+                    </div>
+                <?php endif; ?>
+                <?php 
+                if( !empty( $talent_vimeo_id ) ): ?>
+                    <div style="padding:43.06% 0 0 0;position:relative;width:100%;max-width:100vw;">
+                        <iframe src="https://player.vimeo.com/video/<?php echo esc_html($talent_vimeo_id); ?>?autoplay=1&amp;muted=1&amp;loop=1&amp;autopause=0&amp;controls=0&amp;showinfo=0&amp;title=0&amp;byline=0&amp;portrait=0&amp;quality=1080p" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="REEL_WEB_2023_v2"></iframe>
+                    </div>
+                    <script src="https://player.vimeo.com/api/player.js"></script>
                     <img class="about_header_image" src="<?php echo esc_url($header_image['url']); ?>" />
                 <?php endif; ?>
-            </div>
             <div class="content">
                 <div class="row pb-5">
                     <div class="col col-sm-6 title_1 adieu_light"><?php echo the_field('talent_header'); ?></div>
