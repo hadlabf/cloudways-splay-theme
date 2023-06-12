97025737ab0e979ef7e5723966bb1910f1c9f09c
Hadla Bergman <hadla.bergman@einride.tech>
2023-06-12 22:02:23 +0200

    feat: article page

--- splayonetheme/single-articles.php
+++ splayonetheme/single-articles.php
@@ -1,11 +1,4 @@
 <?php get_header(); ?>
-<!-- 
-	1. HEADER
-	2. INTRO
-	3. KPI
-	4. VIDEOS
-	5. VERTICAL IMAGES
- -->
 <?php 
 	$header_image = get_field('article_header_image');
 	$title = get_field('article_title');
@@ -13,9 +6,10 @@
 	$country = get_field('article_country');
 	$city = get_field('article_city');
 	$text_content = get_field('article_text_content');
-	$foot_text = get_field('article_foot_text');
+	$content_image = get_field('article_content_image');
 	$button_text = get_field('article_button_link_text');
 	$button_url = get_field('article_button_link_url');
+	$foot_text = get_field('article_foot_text');
 ?>
 
 <!-- 1. HEADER -->
@@ -29,12 +23,12 @@
 <!-- 2. INTRO -->
 <div class="content">
 	<div class="row pb-3">
-		<div class="ml-auto col col-sm-5 text_1 pt-4">
+		<div class="ml-auto col col-sm-6 text_1 pt-4">
 			<div class="row">
-				<div class="col p-0">
+				<div class="col">
 					<p class="adieu_light text_2"><?php echo $date; ?></p>
 				</div>
-				<div class="col p-0">
+				<div class="col">
 					<p class="adieu_light text_2 mb-0"><?php echo $country; ?></p>
 					<p class="adieu_light text_2"><?php echo $city; ?></p>
 				</div>
@@ -42,15 +36,36 @@
 		</div>
 	</div>
 	<div class="row pb-5">
-		<div class="col col-sm-6 mr-auto"><h1 style="line-height:1.15;" class="title_1 adieu_light"><?php echo $title; ?></h1></div>
-		<div class="col col-sm-5 text_1 pt-4"><?php echo $text_content; ?></div>
-	</div>
-	<div class="w-100 text-right">
-		<div class="talk_to_container sm_">
-			<a target="_blank" href="<?php echo site_url('/contact');?>" class="cta_link pl-0 pr-4">Read Next </a>
-			<img src="<?php echo get_template_directory_uri(); ?>/images/arrow-icon-black.png"/>
+		<div class="col col-sm-6 mr-auto">
+			<h1 style="line-height:1.15;" class="title_1 adieu_light"><?php echo $title; ?></h1>
+			<?php if( !empty( $content_image ) ): ?>
+				<div style="margin-top:50%;">
+					<img class="w-100 h-auto" src="<?php  echo $content_image["url"]; ?>" alt="<?php echo esc_attr($content_image['alt']); ?>" />
+				</div>
+			<?php endif; ?>
+		</div>
+		<div class="col col-sm-6">
+			<div class="text_1 pt-4"><?php echo $text_content; ?></div>
+			<div class="w-100 pt-5">
+				<div class="talk_to_container sm_ justify-content-end">
+					<a target="_blank" href="<?php echo site_url('/contact');?>" class="cta_link pl-0 pr-4">Read Next </a>
+					<img src="<?php echo get_template_directory_uri(); ?>/images/arrow-icon-black.png"/>
+				</div>
+			</div>
+			<?php if( !empty( $foot_text ) ): ?>
+				<div class="pt-5 d-flex justify-content-start">
+					<p class="small_title"><?php echo $foot_text; ?></p>
+				</div>
+			<?php endif; ?>
+			<?php if( !empty( $button_url ) &&  !empty( $button_text ) ): ?>
+				<div class="pt-3 d-flex justify-content-start">
+					<a href="<?php echo esc_url($button_url);?>" class="primary_button"><?php echo $button_text; ?></a>
+				</div>
+			<?php endif; ?>
 		</div>
 	</div>
+
+
 </div>
 
 <?php get_footer( null, [ 'footer_style' => 'footer_style_secondary' ] ); ?>
