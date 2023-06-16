62226651f3b446f176951608e7893679ab8dc8c2
Hadla Bergman <hadla.bergman@einride.tech>
2023-06-16 10:56:27 +0200

    feat: article page styling updates from feedback

--- splayonetheme/single-articles.php
+++ splayonetheme/single-articles.php
@@ -1,4 +1,4 @@
-<?php get_header( null, [ 'header_style' => 'hide_head' ] ); ?>
+<?php get_header( null, [ 'header_style' => 'single_article' ] ); ?>
 <?php 
 	$header_image = get_field('article_header_image');
 	$title = get_field('article_title');
@@ -10,6 +10,8 @@
 	$button_text = get_field('article_button_link_text');
 	$button_url = get_field('article_button_link_url');
 	$foot_text = get_field('article_foot_text');
+
+	$image_size = getimagesize($content_image['url']);
 ?>
 
 <!-- 1. HEADER -->
@@ -21,51 +23,56 @@
 
 
 <!-- 2. INTRO -->
-<div class="content single_article">
-	<div class="row pb-3">
-		<div class="ml-auto col col-sm-6 text_1 pt-4">
-			<div class="row">
-				<div class="col">
-					<p class="adieu_light text_2"><?php echo $date; ?></p>
-				</div>
-				<div class="col">
-					<p class="adieu_light text_2 mb-0"><?php echo $country; ?></p>
-					<p class="adieu_light text_2"><?php echo $city; ?></p>
+<div class="single_article_page">
+	<div class="content padding_bottom_lg">
+
+		<div class="row pb-3">
+			<div class="ml-auto col col-sm-6 text_1 padding_top_sm">
+				<div class="row">
+					<div class="col">
+						<p class="adieu_light text_2"><?php echo $date; ?></p>
+					</div>
+					<div class="col">
+						<p class="adieu_light text_2 mb-0"><?php echo $country; ?></p>
+						<p class="adieu_light text_2"><?php echo $city; ?></p>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-	<div class="row pb-5">
-		<div class="col col-sm-6 mr-auto">
-			<h1 style="line-height:1.15;" class="title_1 adieu_light pb-3"><?php echo $title; ?></h1>
-			<?php if( !empty( $content_image ) ): ?>
-				<div class="content_image">
-					<img class="w-100 h-auto" src="<?php  echo $content_image["url"]; ?>" alt="<?php echo esc_attr($content_image['alt']); ?>" />
-				</div>
-			<?php endif; ?>
-		</div>
-		<div class="col col-sm-6">
-			<div class="text_1 pt-4"><?php echo $text_content; ?></div>
-			<div class="w-100 pt-5">
-				<div class="talk_to_container sm_ justify-content-end">
-					<a target="_blank" href="<?php echo site_url('/contact');?>" class="cta_link pl-0 pr-4">Read Next </a>
-					<img src="<?php echo get_template_directory_uri(); ?>/images/arrow-thin-icon-white.png"/>
-				</div>
+
+		<div class="row pb-5">
+
+			<div class="col col-sm-6 mr-auto article_column">
+				<h1 style="line-height:1.15;" class="title_1 adieu_light pb-3"><?php echo $title; ?></h1>
+				<?php if( !empty( $content_image ) ): ?>
+					<div class="content_image">
+						<img style="right:calc(100vw - <?php $image_size[1]?>);" class="w-100 h-auto" src="<?php  echo $content_image["url"]; ?>" alt="<?php echo esc_attr($content_image['alt']); ?>" />
+					</div>
+				<?php endif; ?>
 			</div>
-			<?php if( !empty( $foot_text ) ): ?>
-				<div class="pt-5 d-flex justify-content-start w-50">
-					<p class="small_title"><?php echo $foot_text; ?></p>
+
+			<div class="col col-sm-6 article_column">
+				<div class="text_1 pt-4"><?php echo $text_content; ?></div>
+				<div class="w-100 pt-5">
+					<div class="talk_to_container sm_ justify-content-end">
+						<a target="_blank" href="<?php echo site_url('/contact');?>" class="cta_link pl-0 pr-4">Read Next </a>
+						<img src="<?php echo get_template_directory_uri(); ?>/images/arrow-thin-icon-white.png"/>
+					</div>
 				</div>
-			<?php endif; ?>
-			<?php if( !empty( $button_url ) &&  !empty( $button_text ) ): ?>
-				<div class="pt-3 d-flex justify-content-start">
-					<a href="<?php echo esc_url($button_url);?>" class="primary_button"><?php echo $button_text; ?></a>
-				</div>
-			<?php endif; ?>
+				<?php if( !empty( $foot_text ) ): ?>
+					<div class="pt-5 d-flex justify-content-start w-50">
+						<p class="small_title"><?php echo $foot_text; ?></p>
+					</div>
+				<?php endif; ?>
+				<?php if( !empty( $button_url ) &&  !empty( $button_text ) ): ?>
+					<div class="pt-3 d-flex justify-content-start">
+						<a href="<?php echo esc_url($button_url);?>" class="primary_button"><?php echo $button_text; ?></a>
+					</div>
+				<?php endif; ?>
+			</div>
+
 		</div>
+
 	</div>
-
-
 </div>
-
 <?php get_footer( null, [ 'footer_style' => 'footer_style_secondary' ] ); ?>
