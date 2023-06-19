a0b5e1353a29fa896d6f26aab017c98ecde36a00
Hadla Bergman <hadla.bergman@einride.tech>
2023-06-19 11:51:26 +0200

    feat: styling

--- splayonetheme/single-cases.php
+++ splayonetheme/single-cases.php
@@ -39,7 +39,7 @@ if( !empty( $vimeo_id ) ): ?>
 <!-- 2. INTRO -->
 <div class="case_single_page">
 	<div class="content section_padding_3">
-		<h1 class="title_1 adieu_bold pb-5"><?php echo $name?></h1>
+		<h1 class="title_2 adieu_bold pb-5"><?php echo $name?></h1>
 		<div class="d-flex flex-row row">
 			<div class="mobile_full d-flex flex-column col-4">
 				<p class="text_1 bold_1 mb-1">Client:</p>
@@ -68,7 +68,7 @@ if( !empty( $vimeo_id ) ): ?>
 <!-- 4. VIDEOS -->
 <?php if( have_rows('case_videos_list')  ) : ?>
 <div class="content padding_bottom_lg">
-	<div class="d-flex flex-row justify-content-center case_video_list">
+	<div class="justify-content-center case_video_list">
 		<?php while( have_rows('case_videos_list') ): the_row();
 			$video_list_link = get_sub_field('case_videos_list_link');
 			$video_list_image = get_sub_field('case_videos_list_image');
